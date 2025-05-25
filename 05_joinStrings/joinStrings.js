10870266d0e0d4bade0e119d44bdc8d15381918a
Zachary Kowal <zacharykowal31@gmail.com>
2025-05-25 11:40:21 -0400

    Solve JS exercise 5

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -9,6 +9,18 @@
 
 // Add your code right below, good luck!
 
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const thisYear = 1965;
+const birthYear = 1947;
+
+const fullName = firstName + " " + lastName;
+const age = thisYear - birthYear;
+
+const greeting = "Hello! My name is " + fullName + " and I am " + age + " years old.";
+
+console.log(greeting);
+
 
 
 
