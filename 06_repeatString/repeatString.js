66aa8fa91c3921170b7ee557bff6ab4ec9a6fc9d
Zachary Kowal <zacharykowal31@gmail.com>
2025-05-28 15:11:13 -0400

    Solve JS string exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) return "ERROR";
+    result = ""
+    for(let i = 0; i < num; i++) {
+        result += str;
+    }
+    return result;
 };
 
 // Do not edit below this line
