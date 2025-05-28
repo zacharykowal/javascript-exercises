23685a27ed35d5866457d14e0b144b7712a15c6c
Zachary Kowal <zacharykowal31@gmail.com>
2025-05-28 16:11:53 -0400

    Solve JS exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toRemove) {
+    return arr.filter(item => !toRemove.includes(item));
 };
 
 // Do not edit below this line
