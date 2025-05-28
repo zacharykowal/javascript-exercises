7ffb1b36a3a53dd568004e0dec650e904739d4a7
Zachary Kowal <zacharykowal31@gmail.com>
2025-05-28 16:50:09 -0400

    Solve JS exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let tempInCelsius = (temp - 32) * (5 / 9);
+  return (Math.round(tempInCelsius * 10) / 10);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let tempInFahrenheit = (9 / 5) * temp + 32;
+  return (Math.round(tempInFahrenheit * 10) / 10);
 };
 
 // Do not edit below this line
