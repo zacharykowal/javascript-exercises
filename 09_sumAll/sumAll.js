dc5c5f456662da7a17b6c4d500273fb3a43f2d76
Zachary Kowal <zacharykowal31@gmail.com>
2025-05-28 16:26:10 -0400

    Solve JS exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    let result = 0;
+    if (typeof numOne === "number" 
+        && typeof numTwo === "number"
+        && numOne >= 0 
+        && numTwo >= 0 
+        && numOne % 1 === 0 
+        && numTwo % 1 === 0) {
+        if (numOne > numTwo) {
+            for (let i = numTwo; i <= numOne; i++) {
+                result += i;
+            }
+            return result;
+        } else {
+            for (let i = numOne; i <= numTwo; i++) {
+                result += i;
+            }
+            return result;
+        }
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
