f6eae3fd68afd63fe8bc5ca0f07ff80ca9bc3c74
Zachary Kowal <zacharykowal31@gmail.com>
2025-06-08 12:23:39 -0400

    Solve JS exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+
+    for (const obj of arr) {
+        if (typeof obj.yearOfDeath === 'undefined') {
+            obj.yearOfDeath = new Date().getFullYear();
+        }
+    }
+
+    arr.sort((a, b) => (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth));
+    return arr[0];
 
 };
 
