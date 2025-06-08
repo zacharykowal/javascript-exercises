0db3bfad6087ff5ccba45a6f43171e7dfe48c65e
Zachary Kowal <zacharykowal31@gmail.com>
2025-06-08 11:52:16 -0400

    Solve JS exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(target) {
+    let targetNum = Number(target);
+    if (targetNum < 0) {
+        return "OOPS";
+    }
+    if (targetNum === 1 || targetNum === 0){
+        return targetNum;
+    }
+    return fibonacci(targetNum - 1) + fibonacci(targetNum - 2);
 };
 
 // Do not edit below this line
