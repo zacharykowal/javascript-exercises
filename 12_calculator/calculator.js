8729e860481cc42ed01237e9d297a74dc0101425
Zachary Kowal <zacharykowal31@gmail.com>
2025-06-08 11:20:06 -0400

    Solve JS exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, curr) => {return acc + curr}, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, curr) => {return acc * curr}, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) return 1;
+
+	const result = [];
+  for (let i = num; i >= 1; i--) {
+    result.push(i);
+  }
+  return multiply(result);
+  
 };
 
 // Do not edit below this line
