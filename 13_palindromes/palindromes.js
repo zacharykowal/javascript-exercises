d869b6db8b4579290a69f647b861d0ce95ffe655
Zachary Kowal <zacharykowal31@gmail.com>
2025-06-08 11:35:14 -0400

    Solve JS exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let lowerCase = str.toLowerCase();
+    let withoutPunctuation = lowerCase.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, "");
+    let withoutSpaces = withoutPunctuation.replaceAll(" ", "");
+    let reversed = withoutSpaces.split('').reverse().join('');
+    return reversed === withoutSpaces;
 };
 
 // Do not edit below this line
